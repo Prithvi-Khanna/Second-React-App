7fd306b2815495e305ea9303aa5a670500bbf808
Prithvi <prithvikhanna10@gmail.com>
2020-05-10 14:08:23 +0530

    second

---
+++ src/components/counter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <h1>HELLO WORLD</h1>
+        <button>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
