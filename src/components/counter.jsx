efca79364aecdccb7c9af99f2d956e97bf332ed5
Prithvi <prithvikhanna10@gmail.com>
2020-05-11 20:12:28 +0530

    third

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
   render() {
     return (
-      <div>
-        <h1>HELLO WORLD</h1>
+      <React.Fragment>
+        <h1 style={{ fontSize: 80 }}>{2 + "ff"}</h1>
+        <span >{this.formatCount()}</span>
         <button>Increment</button>
-      </div>
+      </React.Fragment>
     );
+  }
+
+  formatCount() {
+    return this.state.count === 0 ? <h2>Zero</h2> : this.state.count;
   }
 }
 
